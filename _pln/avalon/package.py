6baafa03cf81b46d3054cfeaeb8b9515c9835a0c
David Lai <davidlatwe@gmail.com>
2020-08-28 00:11:34 +0800

    Setup Maya

--- _pln/avalon/package.py
+++ _pln/avalon/package.py
@@ -74,9 +74,14 @@ def build_command():
 # Set up environment
 def commands():
     env = globals()["env"]
+    resolve = globals()["resolve"]
     env.PYTHONPATH.prepend("{root}")
 
     # MongoDB
     env.AVALON_MONGO = "{env.HOUSE_PIPELINE_MONGO}"
     env.AVALON_DB = "avalon"
     env.AVALON_TIMEOUT = 5000
+
+    # DCC App Setup
+    if "maya" in resolve:
+        env.PYTHONPATH.append("{root}/setup/maya")
